70f321262f9c1d7f85af75a64b454f92cfca9bd3
Santiago Basulto <santiago.basulto@gmail.com>
2018-12-22 10:47:54 -0300

    Update run_app.py

--- flask_introduction/run_app.py
+++ flask_introduction/run_app.py
@@ -20,5 +20,5 @@ from library._01_simple import app
 if __name__ == '__main__':
     app.debug = True
     host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host=host, port=port)
